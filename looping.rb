ee8bce47a2b7d48f02eb9903b3c171e0f979cd85
vivianne <viviannjoroge91@gmail.com>
2023-02-06 18:56:42 +0300

    commit tree 30e1139f467a1a6728b0245ecce313eca5b3b2cf

--- looping.rb
+++ looping.rb
@@ -1,4 +1,10 @@
 def happy_new_year
+  count = 10
+  while count > 0
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
   # your code here
 end
 
@@ -17,8 +23,28 @@ end
 
 def fizzbuzz_printer
   # your code here
+  for i in 1..100
+    if (i % 3 == 0) && (i % 5 == 0)
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+
 end
 
 def reverse_string(str)
   # your code here
+  result = ""
+  i = str.length - 1
+  while i >= 0
+    result += str[i]
+    i -= 1
+  end
+  result
+  
 end
